20de2d6969ed51d9657f1d783047197f089d758f
Juan Arreguin <juan@arregu.in>
2015-11-03 11:49:09 -0800

    Init

---
+++ Carousel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Carousel
+//
+//  Created by Arreguin, Juan on 11/3/15.
+//  Copyright © 2015 Arreguin, Juan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
